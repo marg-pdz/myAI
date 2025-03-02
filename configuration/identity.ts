5bdf8bd112d0b630c3cf07692e45a1dd9baaef51
marg-pdz <margaux.peduzzi@essec.edu>
2025-03-01 19:58:27 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -2,5 +2,5 @@ export const OWNER_NAME: string = `Margaux`;
 export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
 
 export const AI_NAME: string = `Change advisor`;
-export const AI_TONE: string = `[AI TONE]`;
-export const AI_ROLE: string = `[AI ROLE]`;
+export const AI_TONE: string = `Professional`;
+export const AI_ROLE: string = `Consultant in Change management`;
